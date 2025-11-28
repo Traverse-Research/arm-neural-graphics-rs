e109f001a21869c758fb3643acc3c4445f6430a6
Darius <darius1@live.nl>
2025-11-28 16:47:05 +0100

    Feedback from other pr

--- arm-neural-graphics-lib/src/lib.rs
+++ arm-neural-graphics-lib/src/lib.rs
@@ -1,5 +1,6 @@
 use std::path::{Path, PathBuf};
 
 pub fn dll_path() -> PathBuf {
-    Path::new(env!("CARGO_MANIFEST_DIR")).join("PrebuiltBinary/ngsdk_windows_x64.dll")
+    Path::new(&std::env::var("CARGO_MANIFEST_DIR").unwrap())
+        .join("PrebuiltBinary/ngsdk_windows_x64.dll")
 }
